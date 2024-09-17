3bb01e54461c52059cfc667e408b9012ecc17f6a
Siddharth Sahani <sidhucroedde@gmail.com>
2024-09-17 13:09:03 +0530

    feat: task cards

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,8 +1,44 @@
+import { TaskCard } from "@/components/TaskCard";
+
 
 export default function Home() {
+  const tasks = [
+    {
+      title: "Some random title",
+      description: "Testing some things out",
+      completed: true,
+      priority: 2,
+    },
+    {
+      title: "More",
+      description: "Testing some more things out",
+      completed: false,
+      priority: 1,
+    },
+    {
+      title: "No more",
+      description: "",
+      completed: true,
+      priority: 3,
+    },
+    {
+      title: "TOOO more",
+      description: "Warning: Each child in a list should have a unique 'key' prop. Check the top-level render call using <div>. See https://reactjs.org/link/warning-keys for more information.",
+      completed: true,
+      priority: 3,
+    },
+  ]
+
   return (
-    <h1 className="text-center text-4xl font-bold">
-      Share My Tasks
-    </h1>
+    <>
+      <h1 className="text-center text-4xl font-bold">
+        Share My Tasks
+      </h1>
+      <div className="bg-blue-300">
+        {
+          tasks.map((task, index) => <TaskCard key={index} task={task} />)
+        }
+      </div>
+    </>
   );
 }
---
+++ src/components/TaskCard.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+
+
+export function TaskCard({ task }) {
+  return (
+    <div className="bg-cyan-600 rounded-xl my-2 flex py-2 px-2">
+      <button className='w-2/12 bg-cyan-700 mr-2 rounded-lg text-lg'>
+        Status:
+        <div className={`text-xl ${task.completed ? "text-green-300" : "text-rose-300"}`}>
+          {
+            task.completed ? "Completed" : "Pending"
+          }
+        </div>
+      </button>
+      <div className="w-8/12 flex flex-col">
+        <div className="flex">
+          <div className="w-1/5 text-2xl font-bold">Title:</div>
+          <div className="w-4/5 text-2xl">{task.title}</div>
+        </div>
+        <hr className='opacity-40 my-1'/>
+        <div className="flex">
+          <div className="w-1/5 text-xl font-bold">Desc:</div>
+          <div className="w-4/5 text-xl">{task.description ? task.description : <i>No description provided</i>}</div>
+        </div>
+        <div className="flex">
+          <div className="w-1/5 text-xl font-bold">Priority:</div>
+          <div className="w-4/5 text-xl">
+            {
+              (task.priority === 1) ? <div className="text-xl text-lime-300">Low</div> :
+              (task.priority === 2) ? <div className="text-xl text-orange-300">Medium</div> :
+              (task.priority === 3) ? <div className="text-xl text-red-300">High</div> : null
+            }
+          </div>
+        </div>
+      </div>
+      <button className='w-2/12 bg-red-500 font-bold text-2xl ml-2 rounded-lg'>X</button>
+    </div>
+  )
+}
