3f535f9e6ae7f1fe3124324223423893483144d8
Siddharth Sahani <sidhucroedde@gmail.com>
2024-09-26 22:47:21 +0530

    chore: changes to ui

--- src/components/NewTaskForm.jsx
+++ src/components/NewTaskForm.jsx
@@ -27,17 +27,21 @@ export function NewTaskForm({ addTaskHandler }) {
   const priorityOnChange = (e) => setTaskPriotity(parseInt(e.target.value, 10))
 
   return (
-    <div className="flex mx-2">
-      <div className="flex flex-col w-10/12 mr-2">
-        <input className="bg-gray-700 rounded-md text-md m-0.5 py-1 text-center" type="text" placeholder="Enter task title" value={taskTitle || ''} onChange={titleOnChange} />
-        <input className="bg-gray-700 rounded-md text-md m-0.5 py-1 text-center" type="text" placeholder="Enter task description" value={taskDescription || ''} onChange={descOnChange} />
-        <select className="bg-gray-700 rounded-md text-md m-0.5 py-1 text-center" value={taskPriority} onChange={priorityOnChange}>
-          <option value="1">Low</option>
-          <option value="2">Medium</option>
-          <option value="3">High</option>
-        </select>
+    <div className="bg-[#2d3746] rounded-xl p-2">
+      <div className="text-xl font-semibold px-2">New Task Details:</div>
+      <hr className="opacity-40 m-2 mt-1" />
+      <div className="flex">
+        <div className="flex flex-col w-10/12 mr-2">
+          <input className="bg-gray-600 rounded-md text-md m-0.5 py-1 text-center" type="text" placeholder="Enter task title" value={taskTitle || ''} onChange={titleOnChange} />
+          <input className="bg-gray-600 rounded-md text-md m-0.5 py-1 text-center" type="text" placeholder="Enter task description" value={taskDescription || ''} onChange={descOnChange} />
+          <select className="bg-gray-600 rounded-md text-md m-0.5 py-1 text-center" value={taskPriority} onChange={priorityOnChange}>
+            <option value="1">Low</option>
+            <option value="2">Medium</option>
+            <option value="3">High</option>
+          </select>
+        </div>
+        <button className="bg-green-400 w-2/12 text-3xl font-extrabold mr-1 rounded-md" onClick={createTask}>+</button>
       </div>
-      <button className="bg-green-400 w-2/12 text-3xl font-extrabold mr-1 rounded-md" onClick={createTask}>+</button>
     </div>
   )
 }
--- src/components/TaskCard.jsx
+++ src/components/TaskCard.jsx
@@ -1,8 +1,8 @@
 
 export function TaskCard({ task, toggleStatusHandler, deleteTaskHandler, editable }) {
   return (
-    <div className="bg-cyan-600 rounded-xl my-2 flex py-2 px-2">
-      <button className='w-2/12 bg-cyan-700 mr-2 rounded-lg text-lg' onClick={toggleStatusHandler}>
+    <div className="bg-cyan-600 rounded-xl my-2 flex p-2">
+      <button className='w-2/12 bg-cyan-700 px-2 mr-3 rounded-lg text-lg' onClick={toggleStatusHandler}>
         Status:
         <div className={`text-xl ${task.completed ? "text-green-300" : "text-rose-300"}`}>
           {
@@ -10,19 +10,23 @@ export function TaskCard({ task, toggleStatusHandler, deleteTaskHandler, editabl
           }
         </div>
       </button>
-      <div className={`${editable ? "w-8/12" : "w-10/12"} flex flex-col`}>
+      <div className="w-10/12 flex flex-col">
         <div className="flex">
-          <div className="w-1/5 text-2xl font-bold">Title:</div>
-          <div className="w-4/5 text-2xl">{task.title}</div>
+          <div className="w-2/12 text-2xl font-bold">Title:</div>
+          <div className="w-8/12 text-2xl">{task.title}</div>
+          {
+            editable &&
+            <button className='w-2/12 bg-rose-500 text-xs rounded-lg' onClick={deleteTaskHandler}>Delete</button>
+          }
         </div>
         <hr className='opacity-40 my-1'/>
         <div className="flex">
-          <div className="w-1/5 text-xl font-bold">Desc:</div>
-          <div className="w-4/5 text-xl">{task.description ? task.description : <i>No description provided</i>}</div>
+          <div className="w-2/12 text-xl font-bold">Desc:</div>
+          <div className="w-10/12 text-xl">{task.description ? task.description : <i>No description provided</i>}</div>
         </div>
         <div className="flex">
-          <div className="w-1/5 text-xl font-bold">Priority:</div>
-          <div className="w-4/5 text-xl">
+          <div className="w-2/12 text-xl font-bold">Priority:</div>
+          <div className="w-10/12 text-xl">
             {
               (task.priority === 1) ? <div className="text-xl text-lime-300">Low</div> :
               (task.priority === 2) ? <div className="text-xl text-orange-300">Medium</div> :
@@ -31,10 +35,6 @@ export function TaskCard({ task, toggleStatusHandler, deleteTaskHandler, editabl
           </div>
         </div>
       </div>
-      {
-        editable &&
-        <button className='w-2/12 bg-red-500 font-bold text-2xl ml-2 rounded-lg' onClick={deleteTaskHandler}>X</button>
-      }
     </div>
   )
 }
